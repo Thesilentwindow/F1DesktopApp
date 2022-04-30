7407e3ca684c02fbe6048dafb4756f97070a8818
thesi <thesi@SKYNET_X>
2022-04-30 22:07:11 +0200

    Add project files.

---
+++ F1DesktopApp/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Net.Sockets;
+
+namespace F1DesktopApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private readonly string _port;
+        private readonly string ipAddress;
+        private string str_x;
+        private string str_y;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            UdpClient udpClient = new UdpClient();
+            
+        }
+    }
+}
